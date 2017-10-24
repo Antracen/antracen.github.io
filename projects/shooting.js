72b57bafd367a4e739d108c504b967ecdb9a4dda
Antracen <martin.wass@hotmail.com>
2017-10-24 18:11:10 +0200

    Add files via upload

--- projects/shooting.js
+++ projects/shooting.js
@@ -1,93 +1,104 @@
 // WARNING THIS CODE SUCKS ASS, WILL UPDATE
 
-var player1;
-var player2;
+var map;
+var players;
 var music;
 
 function setup(){
-		// Canvas
-		createCanvas(700, 400);
-		// Other
-		player1 = new Player(100,100,20,0);
-		player2 = new Player(100,100,20,1);
-		music = new Audio('music.wav');
-		music.volume = 0.2;
-		music.addEventListener('ended', function() {
-    		this.currentTime = 0;
-    		this.play();
-		}, false);
-		music.play();
+	var xSize = 700;
+	var ySize = 400;
+	createCanvas(xSize, ySize);
+	map = new Map(xSize, ySize, "#2d2d2d");
+	players = [];
+	players.push(new Player(20,0,LEFT_ARROW,DOWN_ARROW,RIGHT_ARROW));
+	players.push(new Player(20,1,65,83,68));
+	players.push(new Player(20,2,74,75,76));
+	players.push(new Player(20,3,70,71,72));
+	playMusic();
 }
 
 function draw(){
-	background("#2d2d2d");
-	player1.update();
-	player1.render();
-	player2.update();
-	player2.render();
+	map.render();
+	keyListener();
+	for(var i = 0; i < players.length; i++){
+		players[i].update();
+		players[i].render();
+	}
 }
 
-function Player(x,y,size,num){
+function playMusic(){
+	music = new Audio('music.wav');
+	music.volume = 0.2;
+	music.addEventListener('ended', function() {
+   		this.currentTime = 0;
+  		this.play();
+	}, false);
+	music.play();
+}
+
+function Map(xSize, ySize, color){
+	this.xSize = xSize;
+	this.ySize = ySize;
+	this.color = color;
+	this.render = function(){
+		background(this.color);
+	}
+}
+
+function Player(size,num,leftKey,shootKey,rightKey){
 	this.color = color(random(255),random(255),random(255));
+	this.size = size;
+	this.leftKey = leftKey;
+	this.shootKey = shootKey;
+	this.rightKey = rightKey;
+
 	this.points = 0;
 	this.num = num;
-	this.position = createVector(x, y);
-	this.size = size;
-	this.velocity = createVector(3,0);
+
+	this.position = createVector(random(map.xSize), random(map.ySize));
+	this.velocity = createVector(random(3),random(3)).setMag(3);
+
 	this.bullets = [];
+	this.cooldown = 0;
+
 	this.explosion = [];
 	this.exploded = false;
-	this.cooldown = 0;
+
 	this.shootSound = [];
 	this.shootIndex = 0;
 	for(var i = 0; i < 15; i++){
 		this.shootSound[i] = new Audio('shoot.wav');
 		this.shootSound[i].volume = 0.2;
 	}
+	
 	this.explodeSound = [];
 	this.explodeIndex = 0;
 	for(var i = 0; i < 5; i++){
 		this.explodeSound[i] = new Audio('explode.wav');
 		this.explodeSound[i].volume = 0.2;
 	}
-	
-	if(this.num == 0){
-		this.velocity.mult(-1);
-		this.position.add(40,100);
-	}
 
 	this.update = function(){
 		if(this.exploded == false) {
-			if(this.num == 0){
-				this.updatePosition1();
-				this.collision1();
-			} else{
-				this.updatePosition2();
-				this.collision2();
-			}
+			this.updatePosition();
+			this.collision();
 			this.updateBullets();
 		}
 		this.updateExplode();
 	}
 
-	this.collision1 = function(){
+	this.collision = function(){
 
-		for(var i = 0; i < player2.bullets.length; i++){
-			if(this.position.dist(player2.bullets[i][0]) < 15){
-				player2.points++;
-				player2.bullets.splice(i,1);
-				this.explode();
-			}
-		}
-	}
-	this.collision2 = function(){
-
-		for(var i = 0; i < player1.bullets.length; i++){
-			if(this.position.dist(player1.bullets[i][0]) < 15){
-				player1.points++;
-				player1.bullets.splice(i,1);
-				this.explode();
-			}
+		for(var i = 0; i < players.length; i++){
+			if(i == this.num) continue;
+			
+			for(var j = 0; j < players[i].bullets.length; j++){
+				if(this.position.dist(players[i].bullets[j][0]) < 15){
+					players[i].points++;
+					players[i].bullets.splice(i,1);
+					this.explode();
+				}
+			}	
 		}
 	}
 
@@ -114,39 +125,7 @@ function Player(x,y,size,num){
 		}
 	}
 
-	this.updatePosition1 = function(){
-		if(keyIsDown(LEFT_ARROW)){
-			this.velocity.rotate(-0.1);
-		}
-		if(keyIsDown(RIGHT_ARROW)){
-			this.velocity.rotate(0.1);
-		}
-		if(keyIsDown(DOWN_ARROW)){
-			this.fire();
-		}
-		this.position.add(this.velocity);
-		if(this.position.x < 0){
-			this.position.x = 700;
-		} else if(this.position.x > 700){
-			this.position.x = 0;
-		}
-		if(this.position.y < 0){
-			this.position.y = 400;
-		} else if(this.position.y > 400){
-			this.position.y = 0;
-		}
-	}
-
-	this.updatePosition2 = function(){
-		if(keyIsDown(65)){
-			this.velocity.rotate(-0.1);
-		}
-		if(keyIsDown(68)){
-			this.velocity.rotate(0.1);
-		}
-		if(keyIsDown(83)){
-			this.fire();
-		}
+	this.updatePosition = function(){
 		this.position.add(this.velocity);
 		if(this.position.x < 0){
 			this.position.x = 700;
@@ -200,18 +179,10 @@ function Player(x,y,size,num){
 	}
 
 	this.render = function(){
-		if(this.num == 0){
-			fill(this.color);
-			stroke(0,0,0,0);
-			textSize(20);
-			text(""+this.points, 10, 370);
-		}
-		else{
-			fill(this.color);
-			stroke(0,0,0,0);
-			textSize(20);
-			text(""+this.points, 650, 370);
-		}
+		fill(this.color);
+		stroke(0,0,0,0);
+		textSize(20);
+		text(""+this.points, 10+this.num*50, 370);
 		if(this.exploded == true){
 			for(var i = 0; i < this.explosion.length; i++){
 				stroke(this.color);
@@ -232,4 +203,17 @@ function Player(x,y,size,num){
 		}
 	}
 }
-//
+
+function keyListener(){
+	for(var i = 0; i < players.length; i++){
+		if(keyIsDown(players[i].leftKey)){
+			players[i].velocity.rotate(-0.1);
+		}
+		if(keyIsDown(players[i].rightKey)){
+			players[i].velocity.rotate(0.1);
+		}
+		if(keyIsDown(players[i].shootKey)){
+			players[i].fire();
+		}
+	}
+}
