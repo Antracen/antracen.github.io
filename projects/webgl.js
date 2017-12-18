0807ae3f7cebae15ecd44485da254cf575fa0489
Martin Wass <mawass@kth.se>
2017-12-18 22:28:47 +0100

    WEBGL flight test

--- projects/webgl.js
+++ projects/webgl.js
@@ -5,13 +5,36 @@ function setup(){
 	frameRate(50);
 }
 
-var z = 0;
+var speed = 2;
+var direction = new p5.Vector(0,0,-1);
+var xPos = 0;
+var zPos = 0;
+var yPos = 0;
 function draw(){
+
+	if(keyIsDown(UP_ARROW)){
+		var yz = new p5.Vector(direction.y, direction.z);
+		yz.rotate(-0.01);
+		direction.y = yz.x;
+		direction.z = yz.y;
+	}
+	else if(keyIsDown(DOWN_ARROW)){
+		var yz = new p5.Vector(direction.y, direction.z);
+		yz.rotate(0.01);
+		direction.y = yz.x;
+		direction.z = yz.y;
+	}
+	if(keyIsDown(32)){
+		xPos += direction.x;
+		yPos -= direction.y;	
+		zPos += direction.z;
+	}
+
 	pointLight(255, 255, 255, 0, 0, 0);
 	specularMaterial(250, 0, 0);
-	background(255);
-	rotateX(mouseY*0.01);
-	rotateY(-mouseX*0.01);
+	background(230);
+	translate(xPos,yPos,zPos);
+	if(direction.y < 0) rotateX(-p5.Vector.angleBetween(new p5.Vector(0,0,-1), direction));
+	else rotateX(p5.Vector.angleBetween(new p5.Vector(0,0,-1), direction));
 	box(100,100,100,100,100);
-	z += 0.01;
 }
