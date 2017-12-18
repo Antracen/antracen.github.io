2aaaa830c28080c51d45481ef8925baaa5bab272
Martin Wass <mawass@kth.se>
2017-12-18 22:48:36 +0100

    some more webgl

--- projects/webgl.js
+++ projects/webgl.js
@@ -2,10 +2,11 @@ function setup(){
 	var xSize = 500;
 	var ySize = 500;
 	createCanvas(xSize, ySize, WEBGL);
-	frameRate(50);
+	frameRate(60);
 }
 
-var speed = 2;
+var rot = 0;
+var speed = 4;
 var direction = new p5.Vector(0,0,-1);
 var xPos = 0;
 var zPos = 0;
@@ -14,20 +15,26 @@ function draw(){
 
 	if(keyIsDown(UP_ARROW)){
 		var yz = new p5.Vector(direction.y, direction.z);
-		yz.rotate(-0.01);
+		yz.rotate(-0.05);
 		direction.y = yz.x;
 		direction.z = yz.y;
 	}
 	else if(keyIsDown(DOWN_ARROW)){
 		var yz = new p5.Vector(direction.y, direction.z);
-		yz.rotate(0.01);
+		yz.rotate(0.05);
 		direction.y = yz.x;
 		direction.z = yz.y;
 	}
+	if(keyIsDown(LEFT_ARROW)){
+		rot += 0.05;
+	}
+	else if(keyIsDown(RIGHT_ARROW)){
+		rot -= 0.05;
+	}
 	if(keyIsDown(32)){
-		xPos += direction.x;
-		yPos -= direction.y;	
-		zPos += direction.z;
+		xPos += speed*direction.x;
+		yPos -= speed*direction.y;	
+		zPos += speed*direction.z;
 	}
 
 	pointLight(255, 255, 255, 0, 0, 0);
@@ -36,5 +43,6 @@ function draw(){
 	translate(xPos,yPos,zPos);
 	if(direction.y < 0) rotateX(-p5.Vector.angleBetween(new p5.Vector(0,0,-1), direction));
 	else rotateX(p5.Vector.angleBetween(new p5.Vector(0,0,-1), direction));
+	rotateZ(rot);
 	box(100,100,100,100,100);
 }
