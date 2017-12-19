bd1ee8552881b423e19a476e0fed8c3471b88268
Martin Wass <mawass@kth.se>
2017-12-19 20:04:01 +0100

    Working engine

--- projects/3d.js
+++ projects/3d.js
@@ -1,87 +1,74 @@
 var z = 1;
 var lines = [];
 var points = [];
-var walls = [];
-var xSpeed = 0.1;
-var ySpeed = 4;
+var xSpeed = 5;
+var ySpeed = 5;
+var rotSpeed = 0.05;
+var xSize = 300;
+var ySize = 300;
 
 function setup(){
-	var xSize = 200;
-	var ySize = 200;
 	var canvas = createCanvas(xSize, ySize);
 	canvas.position(100, 300);
-	frameRate(100);
 	
 	// FRONT WALL POINTS
-	points.push(new PolarPoint(-10, -100, -100)); // TOP LEFT 0
-	points.push(new PolarPoint(-10, 100, -100)); // TOP RIGHT 1
-	points.push(new PolarPoint(-10, -100, 100)); // BOTTOM LEFT 2
-	points.push(new PolarPoint(-10, 100, 100)); // BOTTOM RIGHT 3
+	points.push(new PolarPoint(-310, -100, -100)); // TOP LEFT 0
+	points.push(new PolarPoint(-310, 100, -100)); // TOP RIGHT 1
+	points.push(new PolarPoint(-310, -100, 100)); // BOTTOM LEFT 2
+	points.push(new PolarPoint(-310, 100, 100)); // BOTTOM RIGHT 3
 	
 	// RIGHT WALL POINTS
-	points.push(new PolarPoint(-13, 100, -100)); // TOP RIGHT 4
-	points.push(new PolarPoint(-13, 100, 100)); // BOTTOM RIGHT 5
+	points.push(new PolarPoint(-510, 100, -100)); // TOP RIGHT 4
+	points.push(new PolarPoint(-510, 100, 100)); // BOTTOM RIGHT 5
 	
 	// BACK WALL POINTS
-	points.push(new PolarPoint(-13, -100, -100)); // TOP RIGHT 6
-	points.push(new PolarPoint(-13, -100, 100)); // BOTTOM RIGHT 7
+	points.push(new PolarPoint(-510, -100, -100)); // TOP RIGHT 6
+	points.push(new PolarPoint(-510, -100, 100)); // BOTTOM RIGHT 7
 	
 	// FRONT WALL LINES
-	lines.push(new Line(0,1)); // TOP 0
-	lines.push(new Line(0,2)); // LEFT 1
-	lines.push(new Line(1,3)); // RIGHT 2
-	lines.push(new Line(2,3)); // DOWN 3
+	lines.push(new Line(0,1)); // TOP
+	lines.push(new Line(0,2)); // LEFT
+	lines.push(new Line(1,3)); // RIGHT
+	lines.push(new Line(2,3)); // DOWN
 	
 	// RIGHT WALL LINES
-	lines.push(new Line(1,4)); // TOP 4
-	lines.push(new Line(3,5)); // BOTTOM 5
-	lines.push(new Line(4,5)); // RIGHT 6
+	lines.push(new Line(1,4)); // TOP
+	lines.push(new Line(3,5)); // BOTTOM
+	lines.push(new Line(4,5)); // RIGHT
 	
 	// BACK WALL LINES
-	lines.push(new Line(4,6)); // TOP 7
-	lines.push(new Line(5,7)); // BOTTOM 8
-	lines.push(new Line(6,7)); // RIGHT 9
+	lines.push(new Line(4,6)); // TOP
+	lines.push(new Line(5,7)); // BOTTOM
+	lines.push(new Line(6,7)); // RIGHT
 	
-	walls.push(new Wall(0,1,2,3));
-	walls.push(new Wall(2,4,5,6));
-	walls.push(new Wall(6,7,8,9));
+	// LEFT WALL LINES
+	lines.push(new Line(0,6)); // BOTTOM
+	lines.push(new Line(2,7)); // RIGHT
 }
 
 function draw(){
 	background(200);
 
 	if(keyIsDown(87)){ // W
-		for(var p of points){
-			p.changeX(xSpeed);
-		}
+		for(var p of points) p.changeX(xSpeed);
 	}
 	else if(keyIsDown(83)){ // S
-		for(var p of points){
-			p.changeX(-xSpeed);
-		}			
+		for(var p of points) p.changeX(-xSpeed);			
 	}
 	if(keyIsDown(65)){ // A
-		for(var p of points){
-			p.changeY(ySpeed);
-		}			
+		for(var p of points) p.changeY(ySpeed);			
 	}
 	else if(keyIsDown(68)){ // D
-		for(var p of points){
-			p.changeY(-ySpeed);
-		}			
+		for(var p of points) p.changeY(-ySpeed);			
 	}
 	
 	if(keyIsDown(LEFT_ARROW)){
-		for(var p of points){
-			p.rotate(-0.002);
-		}
+		for(var p of points) p.rotate(-rotSpeed);
 	}
 	else if(keyIsDown(RIGHT_ARROW)){
-		for(var p of points){
-			p.rotate(0.002);
-		}
+		for(var p of points) p.rotate(rotSpeed);
 	}
-	for(var w of walls) w.render();
+	for(var l of lines) l.render();
 }
 
 function PolarPoint(x, y, z){
@@ -151,29 +138,21 @@ function Line(p1, p2){
 	this.p2 = p2;
 	
 	this.render = function(){
-		if(points[this.p1].getX() > 0 && points[this.p2].getX() < 0){
-			var vector = [points[this.p1].getX() - points[this.p2].getX(), points[this.p1].getY() - points[this.p2].getY(), points[this.p1].getZ() - points[this.p2].getZ()]
-			var k = -points[this.p1].getX()/vector[0];
-			var point = [points[this.p1].getX() + vector[0]*k, points[this.p1].getY() + vector[1]*k, points[this.p1].getZ() + vector[2]*k];
-			line(100 + point[1], 100 + point[2], 100+points[this.p2].getY()/-points[this.p2].getX(), 100+points[this.p2].getZ()/-points[this.p2].getX());
+		var p1Coords = [points[this.p1].getX(), points[this.p1].getY(), points[this.p1].getZ()];
+		var p2Coords = [points[this.p2].getX(), points[this.p2].getY(), points[this.p2].getZ()];
+		if(p1Coords[0] > 0 && p2Coords[0] < 0){
+			var vector = [p1Coords[0] - p2Coords[2], p1Coords[1] - p2Coords[1], p1Coords[2] - p2Coords[2]]
+			var k = -p1Coords[0]/vector[0];
+			var point = [p1Coords[0] + vector[0]*k, p1Coords[1] + vector[1]*k, p1Coords[2] + vector[2]*k];
+			line(xSize/2 + xSize*point[1], ySize/2 + -ySize*point[2], xSize/2 + xSize*points[this.p2].getY()/-p2Coords[0], ySize/2 + -ySize*p2Coords[2]/-p2Coords[0]);
 		}
-		else if(points[this.p1].getX() < 0 && points[this.p2].getX() > 0){
-			var vector = [points[this.p2].getX() - points[this.p1].getX(), points[this.p2].getY() - points[this.p1].getY(), points[this.p2].getZ() - points[this.p1].getZ()]
-			var k = -points[this.p2].getX()/vector[0];
-			var point = [points[this.p2].getX() + vector[0]*k, points[this.p2].getY() + vector[1]*k, points[this.p2].getZ() + vector[2]*k];
-			line(100 + point[1], 100 + point[2], 100+points[this.p1].getY()/-points[this.p1].getX(), 100+points[this.p1].getZ()/-points[this.p1].getX());
+		else if(p1Coords[0] < 0 && p2Coords[0] > 0){
+			var vector = [p2Coords[0] - p1Coords[0], p2Coords[1] - p1Coords[1], p2Coords[2] - p1Coords[2]]
+			var k = -p2Coords[0]/vector[0];
+			var point = [p2Coords[0] + vector[0]*k, p2Coords[1] + vector[1]*k, p2Coords[2] + vector[2]*k];
+			line(xSize/2 + xSize*point[1], ySize/2 + -ySize*point[2], xSize/2 + xSize*p1Coords[1]/-p1Coords[0], ySize/2 + -ySize*p1Coords[2]/-p1Coords[0]);
 		}
-		else if(points[this.p1].getX() > 0 && points[this.p2].getX() > 0) return;
-		else line(100 + points[this.p1].getY()/-points[this.p1].getX(), 100 + points[this.p1].getZ()/-points[this.p1].getX(), 100+points[this.p2].getY()/-points[this.p2].getX(), 100+points[this.p2].getZ()/-points[this.p2].getX());
-	}
-	
-}
-
-function Wall(l1, l2, l3, l4){
-
-	this.lines = [l1,l2,l3,l4];
-
-	this.render = function(){
-		for(var l of this.lines) lines[l].render();
+		else if(p1Coords[0] > 0 && p2Coords[0] > 0) return; // Don't draw behind camera.
+		else line(xSize/2 + xSize*p1Coords[1]/-p1Coords[0], xSize/2 + -ySize*p1Coords[2]/-p1Coords[0], ySize/2 + xSize*p2Coords[1]/-p2Coords[0], ySize/2 + -ySize*p2Coords[2]/-p2Coords[0]);
 	}
 }
