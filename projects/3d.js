23b97b687f84e1d16d6e876800debfa2a1b8a5a0
Martin Wass <mawass@kth.se>
2017-12-20 22:10:19 +0100

    3D needs raycasting

--- projects/3d.js
+++ projects/3d.js
@@ -1,6 +1,6 @@
 var z = 1;
-var lines = [];
 var points = [];
+var walls = [];
 var xSpeed = 5;
 var ySpeed = 5;
 var rotSpeed = 0.05;
@@ -11,67 +11,47 @@ function setup(){
 	var canvas = createCanvas(xSize, ySize);
 	canvas.position(100, 300);
 	
-	// FRONT WALL POINTS
-	points.push(new PolarPoint(-310, -100, -100)); // TOP LEFT 0
-	points.push(new PolarPoint(-310, 100, -100)); // TOP RIGHT 1
-	points.push(new PolarPoint(-310, -100, 100)); // BOTTOM LEFT 2
-	points.push(new PolarPoint(-310, 100, 100)); // BOTTOM RIGHT 3
+	// WALL POINTS
+	// DEPTH, RIGHT/LEFT, UP/DOWN
+	points.push(new SphericalPoint(-510, -100, -100)); // 0 (FLD)
+	points.push(new SphericalPoint(-510, 100, -100)); // 1 (FRD)
+	points.push(new SphericalPoint(-510, 100, 100)); // 2 (FRU)
+	points.push(new SphericalPoint(-510, -100, 100)); // 3 (FLU)
 	
-	// RIGHT WALL POINTS
-	points.push(new PolarPoint(-510, 100, -100)); // TOP RIGHT 4
-	points.push(new PolarPoint(-510, 100, 100)); // BOTTOM RIGHT 5
+	points.push(new SphericalPoint(-910, 100, -100)); // 4 (BRD)
+	points.push(new SphericalPoint(-910, 100, 100)); // 5 (BRU)
+	points.push(new SphericalPoint(-910, -100, -100)); // 6 (BLD)
+	points.push(new SphericalPoint(-910, -100, 100)); // 7 (BLU)
 	
-	// BACK WALL POINTS
-	points.push(new PolarPoint(-510, -100, -100)); // TOP RIGHT 6
-	points.push(new PolarPoint(-510, -100, 100)); // BOTTOM RIGHT 7
+	points.push(new SphericalPoint(-1110, -30, -100)); // 8 (DL)
+	points.push(new SphericalPoint(-1110, 170, -100)); // 9 (DR)
+	points.push(new SphericalPoint(-1110, 170, 100)); // 10 (UR)
+	points.push(new SphericalPoint(-1110, -30, 100)); // 11 (UL)
 	
-	// FRONT WALL LINES
-	lines.push(new Line(0,1)); // TOP
-	lines.push(new Line(0,2)); // LEFT
-	lines.push(new Line(1,3)); // RIGHT
-	lines.push(new Line(2,3)); // DOWN
+	walls.push(new Wall(2,5,4,1));
+	walls.push(new Wall(7,3,0,6));
+	walls.push(new Wall(5,10,9,4));
+	walls.push(new Wall(11,7,6,8));
 	
-	// RIGHT WALL LINES
-	lines.push(new Line(1,4)); // TOP
-	lines.push(new Line(3,5)); // BOTTOM
-	lines.push(new Line(4,5)); // RIGHT
-	
-	// BACK WALL LINES
-	lines.push(new Line(4,6)); // TOP
-	lines.push(new Line(5,7)); // BOTTOM
-	lines.push(new Line(6,7)); // RIGHT
-	
-	// LEFT WALL LINES
-	lines.push(new Line(0,6)); // BOTTOM
-	lines.push(new Line(2,7)); // RIGHT
 }
 
 function draw(){
 	background(200);
-
-	if(keyIsDown(87)){ // W
-		for(var p of points) p.changeX(xSpeed);
-	}
-	else if(keyIsDown(83)){ // S
-		for(var p of points) p.changeX(-xSpeed);			
-	}
-	if(keyIsDown(65)){ // A
-		for(var p of points) p.changeY(ySpeed);			
-	}
-	else if(keyIsDown(68)){ // D
-		for(var p of points) p.changeY(-ySpeed);			
-	}
 	
-	if(keyIsDown(LEFT_ARROW)){
-		for(var p of points) p.rotate(-rotSpeed);
-	}
-	else if(keyIsDown(RIGHT_ARROW)){
-		for(var p of points) p.rotate(rotSpeed);
-	}
-	for(var l of lines) l.render();
+	// MOVEMENT
+	if(keyIsDown(87)) for(var p of points) p.changeX(xSpeed); // W
+	else if(keyIsDown(83)) for(var p of points) p.changeX(-xSpeed); // S
+	if(keyIsDown(65)) for(var p of points) p.changeY(ySpeed); // A
+	else if(keyIsDown(68)) for(var p of points) p.changeY(-ySpeed); // D	
+	
+	// CAMERA
+	if(keyIsDown(LEFT_ARROW)) for(var p of points) p.rotate(-rotSpeed); 
+	else if(keyIsDown(RIGHT_ARROW)) for(var p of points) p.rotate(rotSpeed); 
+	
+	for(var w of walls) w.render();
 }
 
-function PolarPoint(x, y, z){
+function SphericalPoint(x, y, z){
 	this.len = sqrt(x*x + y*y + z*z);
 	this.phi = acos(z/this.len);
 	this.theta = atan2(y,x);
@@ -93,17 +73,9 @@ function PolarPoint(x, y, z){
 	
 	this.angleCheck();
 	
-	this.getX = function(){
-		return this.len * sin(this.phi) * cos(this.theta);
-	}
-	
-	this.getY = function(){
-		return this.len * sin(this.phi) * sin(this.theta);
-	}
-	
-	this.getZ = function(){
-		return this.len * cos(this.phi);
-	}
+	this.getX = function(){ return this.len * sin(this.phi) * cos(this.theta); }
+	this.getY = function(){ return this.len * sin(this.phi) * sin(this.theta); }
+	this.getZ = function(){ return this.len * cos(this.phi); }
 	
 	this.changeX = function(value){
 		var x = this.getX();
@@ -133,26 +105,38 @@ function PolarPoint(x, y, z){
 	}
 }
 
-function Line(p1, p2){
-	this.p1 = p1;
-	this.p2 = p2;
+function Wall(topleft, topright, bottomright, bottomleft){
+
+	this.lines = [[topleft,topright], [bottomright,bottomleft]];
 	
 	this.render = function(){
-		var p1Coords = [points[this.p1].getX(), points[this.p1].getY(), points[this.p1].getZ()];
-		var p2Coords = [points[this.p2].getX(), points[this.p2].getY(), points[this.p2].getZ()];
-		if(p1Coords[0] > 0 && p2Coords[0] < 0){
-			var vector = [p1Coords[0] - p2Coords[2], p1Coords[1] - p2Coords[1], p1Coords[2] - p2Coords[2]]
-			var k = -p1Coords[0]/vector[0];
-			var point = [p1Coords[0] + vector[0]*k, p1Coords[1] + vector[1]*k, p1Coords[2] + vector[2]*k];
-			line(xSize/2 + xSize*point[1], ySize/2 + -ySize*point[2], xSize/2 + xSize*points[this.p2].getY()/-p2Coords[0], ySize/2 + -ySize*p2Coords[2]/-p2Coords[0]);
+		beginShape();
+	
+		for(var l of this.lines){
+			var p1Coords = [points[l[0]].getX(), points[l[0]].getY(), points[l[0]].getZ()];
+			var p2Coords = [points[l[1]].getX(), points[l[1]].getY(), points[l[1]].getZ()];
+			
+			if(p1Coords[0] > 0 && p2Coords[0] > 0) return; // Don't draw behind camera.
+			
+			if(p1Coords[0] > 0 && p2Coords[0] < 0){ // Starting points behind player.
+				var vector = [p2Coords[0] - p1Coords[0], p2Coords[1] - p1Coords[1], p2Coords[2] - p1Coords[2]];
+				var k = -p1Coords[0]/vector[0];
+				var point1 = [p1Coords[0] + vector[0]*k, p1Coords[1] + vector[1]*k, p1Coords[2] + vector[2]*k];
+				vertex(xSize/2 + xSize*point1[1], ySize/2 + -ySize*point1[2]);
+				vertex(xSize/2 + xSize*p2Coords[1]/-p2Coords[0], ySize/2 + -ySize*p2Coords[2]/-p2Coords[0]);
+			}
+			else if(p1Coords[0] < 0 && p2Coords[0] > 0){ // Ending points behind player.
+				var vector = [p1Coords[0] - p2Coords[0], p1Coords[1] - p2Coords[1], p1Coords[2] - p2Coords[2]]
+				var k = -p2Coords[0]/vector[0];
+				var point2 = [p2Coords[0] + vector[0]*k, p2Coords[1] + vector[1]*k, p2Coords[2] + vector[2]*k];
+				vertex(xSize/2 + xSize*p1Coords[1]/-p1Coords[0], ySize/2 + -ySize*p1Coords[2]/-p1Coords[0]);
+				vertex(xSize/2 + xSize*point2[1], ySize/2 + -ySize*point2[2]);
+			}
+			else{ // Everything in front of player.
+				vertex(xSize/2 + xSize*p1Coords[1]/-p1Coords[0], ySize/2 + -ySize*p1Coords[2]/-p1Coords[0]);
+				vertex(xSize/2 + xSize*p2Coords[1]/-p2Coords[0], ySize/2 + -ySize*p2Coords[2]/-p2Coords[0]);		
+			}
 		}
-		else if(p1Coords[0] < 0 && p2Coords[0] > 0){
-			var vector = [p2Coords[0] - p1Coords[0], p2Coords[1] - p1Coords[1], p2Coords[2] - p1Coords[2]]
-			var k = -p2Coords[0]/vector[0];
-			var point = [p2Coords[0] + vector[0]*k, p2Coords[1] + vector[1]*k, p2Coords[2] + vector[2]*k];
-			line(xSize/2 + xSize*point[1], ySize/2 + -ySize*point[2], xSize/2 + xSize*p1Coords[1]/-p1Coords[0], ySize/2 + -ySize*p1Coords[2]/-p1Coords[0]);
-		}
-		else if(p1Coords[0] > 0 && p2Coords[0] > 0) return; // Don't draw behind camera.
-		else line(xSize/2 + xSize*p1Coords[1]/-p1Coords[0], xSize/2 + -ySize*p1Coords[2]/-p1Coords[0], ySize/2 + xSize*p2Coords[1]/-p2Coords[0], ySize/2 + -ySize*p2Coords[2]/-p2Coords[0]);
+		endShape(CLOSE);
 	}
 }
