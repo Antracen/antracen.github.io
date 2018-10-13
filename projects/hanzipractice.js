00a45a4019a1a9dd0c1410bf9c7b55c9bee21309
Martin Wass <wass@W530.wass>
2018-10-13 21:00:34 +0200

    New project

--- projects/hanzipractice.js
+++ projects/hanzipractice.js
@@ -1,30 +1,53 @@
 let file_input;
 let txt = "";
+let current_hanzi;
+let current_hanzi_num = -1;
 
 function setup() {
 	textSize(32);
 	let my_canvas = createCanvas(400, 400);
+	current_hanzi = createP("Current hanzi:");
 	createElement('br');
-	let clear_canvas_button = createButton("New hanzi");
+	let clear_canvas_button = createButton("New hanzi (n)");
+	let check_button = createButton("Check answer (c)");
 	background(200);
+	
 	clear_canvas_button.mousePressed(new_hanzi);
+	check_button.mousePressed(check_hanzi);
 	createElement('br');
-	file_input = createInput("", "file");
+	file_input = createFileInput(file_select_func);
+}
+
+function check_hanzi() {
+	current_hanzi.html(txt[current_hanzi_num][0]);
+}
+
+function file_select_func(file) {
+	txt = file.data;
+	txt = txt.split("\n");
+	txt = txt.map((e) => e.split("\t"));
 }
 
 function new_hanzi() {
 	background(200)
 	if(txt != "") {
-		line = random(0, txt.size());
-		text(txt[line], 50, 50);
-	}
-	else if(file_input.value() != "") {
-		txt = loadStrings(file_input.value()); 
+		current_hanzi_num = int(random(0, txt.length));
+		let hanzi_text = txt[current_hanzi_num][1];
+		current_hanzi.html(hanzi_text);
 	}
 }
 
 function draw() {
 	if(mouseIsPressed) {
 		line(mouseX, mouseY, pmouseX, pmouseY);
+	}
+}
+
+function keyPressed() {
+	if (keyCode === 78) {
+		new_hanzi();
+	}
+	else if (keyCode === 67) {
+		check_hanzi();
 	}
 }
