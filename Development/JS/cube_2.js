24c216e033080711e14a0dd122f9cdb34530c2a3
Martin Wass <wass@W530.wass>
2019-04-16 21:44:36 +0200

    New cube project done

--- Development/JS/cube_2.js
+++ Development/JS/cube_2.js
@@ -1,4 +1,3 @@
-// TODO: Rotations of cube
 var cube_points;
 var cube_colors;
 // Which cubies are in which positions on the cube
@@ -86,18 +85,26 @@ function setup() {
 
 	angle = HALF_PI;
 	turn_values = [
-		['Y', -1, [0,1,2,3,4,5,6,7,8],       	[1,3,7,5], 		[0,6,8,2]], 	// U
-		['Y',  1, [0,1,2,3,4,5,6,7,8],       	[1,5,7,3], 		[0,2,8,6]], 	// U'
-		['X',  1, [2,5,8,11,14,17,20,23,26], 	[5,17,23,11], 	[2,8,26,20]], 	// R
-		['X', -1, [2,5,8,11,14,17,20,23,26], 	[5,11,23,17], 	[2,20,26,8]], 	// R'
-		['X', -1, [0,3,6,9,12,15,18,21,24],  	[3,9,21,15], 	[0,18,24,6]], 	// L
-		['X',  1, [0,3,6,9,12,15,18,21,24],  	[3,15,21,9], 	[0,6,24,18]], 	// L'
-		['Z',  1, [0,1,2,9,10,11,18,19,20],  	[1,11,19,9], 	[0,2,20,18]], 	// F
-		['Z', -1, [0,1,2,9,10,11,18,19,20],  	[1,9,19,11], 	[0,18,20,2]], 	// F'
-		['Y',  1, [18,19,20,21,22,23,24,25,26], [19,23,25,21], 	[18,20,26,24]], // D
-		['Y', -1, [18,19,20,21,22,23,24,25,26], [19,21,25,23], 	[18,24,26,20]], // D'
-		['Z', -1, [6,7,8,15,16,17,24,25,26], 	[7,15,25,17], 	[8,6,24,26]], 	// B
-		['Z',  1, [6,7,8,15,16,17,24,25,26], 	[7,17,25,15], 	[8,26,24,6]], 	// B'
+		['Y', -1, [0,1,2,3,4,5,6,7,8],       	[[1,3,7,5]], 		[[0,6,8,2]]], 	// U
+		['Y',  1, [0,1,2,3,4,5,6,7,8],       	[[1,5,7,3]], 		[[0,2,8,6]]], 	// U'
+		['X',  1, [2,5,8,11,14,17,20,23,26], 	[[5,17,23,11]], 	[[2,8,26,20]]], 	// R
+		['X', -1, [2,5,8,11,14,17,20,23,26], 	[[5,11,23,17]], 	[[2,20,26,8]]], 	// R'
+		['X', -1, [0,3,6,9,12,15,18,21,24],  	[[3,9,21,15]], 		[[0,18,24,6]]], 	// L
+		['X',  1, [0,3,6,9,12,15,18,21,24],  	[[3,15,21,9]], 		[[0,6,24,18]]], 	// L'
+		['Z',  1, [0,1,2,9,10,11,18,19,20],  	[[1,11,19,9]], 		[[0,2,20,18]]], 	// F
+		['Z', -1, [0,1,2,9,10,11,18,19,20],  	[[1,9,19,11]], 		[[0,18,20,2]]], 	// F'
+		['Y',  1, [18,19,20,21,22,23,24,25,26], [[19,23,25,21]], 	[[18,20,26,24]]], // D
+		['Y', -1, [18,19,20,21,22,23,24,25,26], [[19,21,25,23]], 	[[18,24,26,20]]], // D'
+		['Z', -1, [6,7,8,15,16,17,24,25,26], 	[[7,15,25,17]], 	[[8,6,24,26]]], 	// B
+		['Z',  1, [6,7,8,15,16,17,24,25,26], 	[[7,17,25,15]], 	[[8,26,24,6]]], 	// B'
+		// Rotations
+		['Y',  -1, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26], [[1,3,7,5],[9,15,17,11],[19,21,25,23],[10,12,16,14]], [[0,6,8,2],[18,24,26,20]]], // Y
+		['Y', 	1, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26], [[1,5,7,3],[9,11,17,15],[19,23,25,21],[10,14,16,12]], [[0,2,8,6],[18,20,26,24]]], // Y'
+		['X',  -1, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26], [[3,9,21,15],[1,19,25,7],[5,11,23,17],[4,10,22,16]], [[0,18,24,6],[2,20,26,8]]], // X
+		['X',   1, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26], [[3,15,21,9],[1,7,25,19],[5,17,23,11],[4,16,22,10]], [[0,6,24,18],[2,8,26,20]]], // X'
+		// Slice moves
+		['X',  1, [1,4,7,10,13,16,19,22,25], 	[[1,7,25,19],[4,16,22,10]], 	[]], 	// M'
+		['X', -1, [1,4,7,10,13,16,19,22,25], 	[[1,19,25,7],[4,10,22,16]], 	[]], 	// M
 	];
 
 	turns_performed = [];
@@ -152,7 +159,7 @@ function update_cube() {
 	var dir = current_turn;
 	var face_num, axis, theta;
 
-	var ticks_per_turn = 10;
+	var ticks_per_turn = 8;
 
 	axis = turn_values[dir][0];
 	theta = turn_values[dir][1] * PI/(2*ticks_per_turn);
@@ -161,7 +168,7 @@ function update_cube() {
 	corner_cycle = turn_values[dir][4];
 
 	// Matrix transformations
-	for(var i = 0; i < 9; i++) {
+	for(var i = 0; i < face_cubies.length; i++) {
 		var face_points = cube_points[cubie_positions[face_cubies[i]]];
 		for(var j = 0; j < 8; j++) {
 			var point = face_points[j];
@@ -190,15 +197,22 @@ function update_cube() {
 
 	current_turn_ticks++;
 
+	// Update cubie positions when the turn is finished
 	if(current_turn_ticks == ticks_per_turn) {
-		var last_edge = cubie_positions[edge_cycle[3]];
-		var last_corner = cubie_positions[corner_cycle[3]];
-		for(var i = 3; i > 0; i--) {
-			cubie_positions[edge_cycle[i]] = cubie_positions[edge_cycle[i-1]];
-			cubie_positions[corner_cycle[i]] = cubie_positions[corner_cycle[i-1]];
+		for(var i = 0; i < edge_cycle.length; i++) {
+			var last_edge = cubie_positions[edge_cycle[i][3]];
+			for(var j = 3; j > 0; j--) {
+				cubie_positions[edge_cycle[i][j]] = cubie_positions[edge_cycle[i][j-1]];
+			}
+			cubie_positions[edge_cycle[i][0]] = last_edge;
 		}
-		cubie_positions[edge_cycle[0]] = last_edge;
-		cubie_positions[corner_cycle[0]] = last_corner;
+		for(var i = 0; i < corner_cycle.length; i++) {
+			var last_corner = cubie_positions[corner_cycle[i][3]];
+			for(var j = 3; j > 0; j--) {
+				cubie_positions[corner_cycle[i][j]] = cubie_positions[corner_cycle[i][j-1]];
+			}
+			cubie_positions[corner_cycle[i][0]] = last_corner;
+		}
 		current_turn = "None";
 		current_turn_ticks = 0;
 	}
@@ -218,6 +232,12 @@ function keyPressed() {
     else if(keyCode == 86)   	turn_code = 9; // V == D'
     else if(keyCode == 87)   	turn_code = 10; // O == B'
 	else if(keyCode == 79)   	turn_code = 11; // W == B
-	else 						return;
+	else if(keyCode == 192)		turn_code = 12; // Ö = Y
+	else if(keyCode == 65)		turn_code = 13; // A = Y'
+	else if(keyCode == 66)		turn_code = 14; // B = X'
+	else if(keyCode == 89)		turn_code = 15;	// Y = X
+	else if(keyCode == 85)		turn_code = 16;	// U = M'
+	else if(keyCode == 77)		turn_code = 17;	// M = M
+	else return;
 	turns_performed.push(turn_code);
 }
